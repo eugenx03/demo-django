99cd4438e9306fb8cc6b8796022203dc1c2ab932
evgenii potapov <eugenx03@gmail.com>
2022-10-04 01:30:09 +0200

    updated workflow

--- users/tests.py
+++ users/tests.py
@@ -4,10 +4,11 @@ from django.contrib.auth.models import User
 
 # Create your tests here.
 
+
 class UserTestCase(TestCase):
     def test_user(self):
-        username = 'shetu'
-        password = 'hello'
+        username = "shetu"
+        password = "hello"
         u = User(username=username)
         u.set_password(password)
         u.save()
