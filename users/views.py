4063a32a07b3ec14d6d8c75bfe4e117a43573abb
evgenii potapov <eugenx03@gmail.com>
2022-10-04 01:25:18 +0200

    updated workflow

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def homepage(request):
-  return render(request, 'index.html', context={})
+    return render(request, 'index.html', context={})
